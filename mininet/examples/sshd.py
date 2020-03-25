38dee678ab53f74ff3e2fa511738f80e76851fb3
Frantisek Flachs <fflachs@seznam.cz>
2020-03-25 22:23:03 +0100

    add mininet project and repaid python 2 and python 3 compatifility

--- mininet/examples/sshd.py
+++ mininet/examples/sshd.py
@@ -26,71 +26,58 @@ from mininet.topolib import TreeTopo
 from mininet.util import waitListening
 
 
-def TreeNet(depth=1, fanout=2):
+def TreeNet( depth=1, fanout=2, **kwargs ):
     "Convenience function for creating tree networks."
-    topo = TreeTopo(depth, fanout)
-    return Mininet(topo)
+    topo = TreeTopo( depth, fanout )
+    return Mininet( topo, **kwargs )
 
-
-def connectToRootNS(network, switch, ip, routes):
+def connectToRootNS( network, switch, ip, routes ):
     """Connect hosts to root namespace via switch. Starts network.
       network: Mininet() network object
       switch: switch to connect to root namespace
       ip: IP address for root namespace node
       routes: host networks to route to"""
-
     # Create a node in root namespace and link to switch 0
-    root = Node('root', inNamespace=False)
-    intf = network.addLink(root, switch).intf1
-    root.setIP(ip, intf=intf)
-
+    root = Node( 'root', inNamespace=False )
+    intf = network.addLink( root, switch ).intf1
+    root.setIP( ip, intf=intf )
     # Start network that now includes link to root namespace
     network.start()
-
     # Add routes from root ns to hosts
     for route in routes:
-        root.cmd('route add -net ' + route + ' dev ' + str(intf))
+        root.cmd( 'route add -net ' + route + ' dev ' + str( intf ) )
 
-
-def sshd(network, cmd='/usr/sbin/sshd', opts='-D',
-         ip='10.123.123.1/32', routes=None, switch=None):
+def sshd( network, cmd='/usr/sbin/sshd', opts='-D',
+          ip='10.123.123.1/32', routes=None, switch=None ):
     """Start a network, connect it to root ns, and run sshd on all hosts.
        ip: root-eth0 IP address in root namespace (10.123.123.1/32)
        routes: Mininet host networks to route to (10.0/24)
        switch: Mininet switch to connect to root namespace (s1)"""
-
     if not switch:
-        switch = network['s1']  # switch to use
+        switch = network[ 's1' ]  # switch to use
     if not routes:
-        routes = ['10.0.0.0/24']
-
-    connectToRootNS(network, switch, ip, routes)
-
+        routes = [ '10.0.0.0/24' ]
+    connectToRootNS( network, switch, ip, routes )
     for host in network.hosts:
-        host.cmd(cmd + ' ' + opts + '&')
-
-    info("*** Waiting for ssh daemons to start\n")
+        host.cmd( cmd + ' ' + opts + '&' )
+    info( "*** Waiting for ssh daemons to start\n" )
     for server in network.hosts:
-        waitListening(server=server, port=22, timeout=5)
+        waitListening( server=server, port=22, timeout=5 )
 
-    info("\n*** Hosts are running sshd at the following addresses:\n")
+    info( "\n*** Hosts are running sshd at the following addresses:\n" )
     for host in network.hosts:
-        info(host.name, host.IP(), '\n')
-    info("\n*** Type 'exit' or control-D to shut down network\n")
-
-    CLI(network)
-
-    info("\n*** Stopping sshd.\n")
+        info( host.name, host.IP(), '\n' )
+    info( "\n*** Type 'exit' or control-D to shut down network\n" )
+    CLI( network )
     for host in network.hosts:
-        host.cmd('kill %' + cmd)
-
+        host.cmd( 'kill %' + cmd )
     network.stop()
 
-
 if __name__ == '__main__':
-    lg.setLogLevel('info')
-    net = TreeNet(depth=1, fanout=4)
+    lg.setLogLevel( 'info')
+    net = TreeNet( depth=1, fanout=4 )
     # get sshd args from the command line or use default args
     # useDNS=no -u0 to avoid reverse DNS lookup timeout
-    argvopts = ' '.join(sys.argv[1:]) if len(sys.argv) > 1 else ('-D -o UseDNS=no -u0')
-    sshd(net, opts=argvopts)
+    argvopts = ' '.join( sys.argv[ 1: ] ) if len( sys.argv ) > 1 else (
+        '-D -o UseDNS=no -u0' )
+    sshd( net, opts=argvopts )

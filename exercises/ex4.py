c767db9cf880552b87bda129f63cff589f908b0d
Frantisek Flachs <fflachs@seznam.cz>
2020-04-05 18:20:23 +0200

    exercise 2 edited (ex4)

--- exercises/ex4.py
+++ exercises/ex4.py
@@ -4,17 +4,13 @@ if __name__ == '__main__':
 
     sdnc = Floodlight()
 
-    # add rules for communication between host h1 and root (computer)
-    print(sdnc.firewallAddRule({"src-ip": "10.0.0.1/32", "dst-ip": "10.123.123.1/32"}))
-    print(sdnc.firewallAddRule({"src-ip": "10.123.123.1/32", "dst-ip": "10.0.0.1/32"}))
-
-    # add rules for communication between host h2 and root (computer)
-    print(sdnc.firewallAddRule({"src-ip": "10.0.0.2/32", "dst-ip": "10.123.123.1/32"}))
-    print(sdnc.firewallAddRule({"src-ip": "10.123.123.1/32", "dst-ip": "10.0.0.2/32"}))
+    print(sdnc.firewallClearRules())
 
     # allow ARP between host h1 and h3
-    print(sdnc.firewallAddRule({"src-ip": "10.0.0.1/32", "dst-ip": "10.0.0.3/32", "dl-type": "ARP"}))
-    print(sdnc.firewallAddRule({"src-ip": "10.0.0.3/32", "dst-ip": "10.0.0.1/32", "dl-type": "ARP"}))
+    # print(sdnc.firewallAddRule({"src-ip": "10.0.0.1/32", "dst-ip": "10.0.0.3/32", "dl-type": "ARP"}))
+    # print(sdnc.firewallAddRule({"src-ip": "10.0.0.3/32", "dst-ip": "10.0.0.1/32", "dl-type": "ARP"}))
+
+    print(sdnc.firewallAddRule({"dl-type": "ARP"}))
 
     # allow ICMP between host h1 and h3
     print(sdnc.firewallAddRule({"src-ip": "10.0.0.1/32", "dst-ip": "10.0.0.3/32", "nw-proto": "ICMP"}))
@@ -30,15 +26,3 @@ if __name__ == '__main__':
 
 
     print(sdnc.firewallListRules())
-
-    flow1 = {
-        'switch': "00:00:00:00:00:00:00:01",
-        "name": "flow_mod_1",
-        "cookie": "0",
-        "priority": "32768",
-        "in_port": "1",
-        "active": "true",
-        "actions": "output=controller"
-    }
-
-    print(sdnc.addFlow(flow1))

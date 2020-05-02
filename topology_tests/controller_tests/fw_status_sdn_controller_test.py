5d0c0b786133c3ec76096c8e1b7bac9a965d18aa
Frantisek Flachs <fflachs@seznam.cz>
2020-05-02 12:44:38 +0200

    added test for FW status

---
+++ topology_tests/controller_tests/fw_status_sdn_controller_test.py
@@ -0,0 +1,28 @@
+from topology_tests.topology_test import TopologyTest
+from sdn_controllers.floodlight import Floodlight
+
+
+class FwStatusSDNControllerTest(TopologyTest):
+    """Testing for SDN Controller firewall status"""
+
+    def execute(self, testParams):
+        """ Execute the test for getting FW status from SDN Controller
+        params: status that we are asking"""
+
+        try:
+            status = testParams[0]
+
+            print(testParams)
+
+            sdnc = Floodlight()
+            # get FW status from SDN controller
+            currentStatus = sdnc.firewallStatus()
+            print(currentStatus['result'])
+            if status in currentStatus['result']:
+                return True
+            else:
+                return False
+
+        except Exception as e:
+            print("Something went wrong " + str(e))
+            return False

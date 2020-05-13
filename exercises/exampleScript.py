f60391c0c7a5863f8798183c4c179260c8f660a6
Frantisek Flachs <fflachs@seznam.cz>
2020-05-13 21:11:48 +0200

    edited changes

--- exercises/exampleScript.py
+++ exercises/exampleScript.py
@@ -1,4 +1,10 @@
+import inspect
+import os
+import sys
 
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
 
 if __name__ == '__main__':
     print("Hello world.")
